bba3dac00e48743ef85e959ecc393c2f41d1e213
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-12-11 22:55:54 +0545

    Enhance GroceryItemCard with quantity controls and update Nav component to display cart item count

--- src/components/GroceryItemCard.tsx
+++ src/components/GroceryItemCard.tsx
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import React from 'react'
 import {motion} from 'motion/react'
 import Image from 'next/image';
-import { PlusCircle, ShoppingCart } from 'lucide-react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '@/redux/store';
+import { Minus, Plus, PlusCircle, ShoppingCart } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/redux/store';
 import { addToCart } from '@/redux/cartSlice';
 
 interface IGrocery {
@@ -21,6 +21,10 @@ interface IGrocery {
 
 function GroceryItemCard({item}:{item:IGrocery}) {
   const dispatch = useDispatch<AppDispatch>();
+
+  const { cartData } = useSelector((state: RootState) => state.cart);
+  const cartItem=cartData.find(i=>i._id==item._id);
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 ,scale:0.9}}
@@ -42,13 +46,27 @@ function GroceryItemCard({item}:{item:IGrocery}) {
           <span className='text-green-700 font-bold text-lg'>रु. {item.price}</span>
         </div>
 
-        <motion.button
-          className='mt-4 flex items-center justify-center cursor-pointer duration-300 gap-2 bg-green-600 hover:bg-green-700 text-white rounded-full py-2 text-sm font-medium transition-all'
-          whileTap={{ scale: 0.95 }}
-          onClick={()=>dispatch(addToCart({...item,quantity:1}))}
-        >
-          <ShoppingCart /> Add to Cart
-        </motion.button>
+        {!cartItem ? 
+          <motion.button
+            className='mt-4 flex items-center justify-center cursor-pointer duration-300 gap-2 bg-green-600 hover:bg-green-700 text-white rounded-full py-2 text-sm font-medium transition-all'
+            whileTap={{ scale: 0.95 }}
+            onClick={()=>dispatch(addToCart({...item,quantity:1}))}>
+            <ShoppingCart /> Add to Cart
+          </motion.button>
+          : 
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+            className='mt-4 flex items-center justify-center bg-green-50 border border-green-200 rounded-full py-2 px-4 gap-4'
+          >
+            <button className='w-7 h-7 flex items-center justify-center cursor-pointer rounded-full bg-green-100 hover:bg-green-200 transition-all'><Minus size={16} className='text-green-700' /></button>
+            <span className='text-sm font-semibold text-gray-800'>{cartItem.quantity}</span>
+            <button className='w-7 h-7 flex items-center justify-center cursor-pointer rounded-full bg-green-100 hover:bg-green-200 transition-all'><Plus size={16} className='text-green-700' /></button>
+          </motion.div>
+        }
+
+        
       </div>
     </motion.div>
   )
